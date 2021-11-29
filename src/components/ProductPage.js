e5a35348e2094e6207d74680c3edb655bb700e1f
simplynormal <nguyenchinhkhoi@gmail.com>
2021-11-29 14:15:05 +0700

    Set logic for search bar

--- src/components/ProductPage.js
+++ src/components/ProductPage.js
@@ -6,13 +6,21 @@ import ProductList from './ProductList'
 
 
 function ProductPage() {
+  const [searchText, setSearchText] = React.useState("")
+
   return (
     <div className="product-container">
       <div className="search">
-        <input type="text" placeholder="Search.." />
+        <input
+          type="text"
+          placeholder="Search.."
+          value={searchText}
+          onChange={(e) => { setSearchText(e.target.value) }}
+        />
         <svg
           viewBox="0 0 24 24"
           xmlns="http://www.w3.org/2000/svg"
+          onClick={() => { setSearchText("") }}
         >
           <path d="M24 2.41714L21.5829 0L12 9.58286L2.41714 0L0 2.41714L9.58286 12L0 21.5829L2.41714 24L12 14.4171L21.5829 24L24 21.5829L14.4171 12L24 2.41714Z" fill="#505050" />
         </svg>
