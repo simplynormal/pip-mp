57eca8b54a8bc41d41a50042fd92543fd06aa8f1
simplynormal <nguyenchinhkhoi@gmail.com>
2021-12-01 16:28:18 +0700

    Add homepage onclick to navigate to detail

--- src/components/Home.js
+++ src/components/Home.js
@@ -138,6 +138,7 @@ export default class Home extends Component {
                 hover[index] = false;
                 this.setState({ hover: hover });
               }}
+              onClick={() => window.location.href = '/detail'}
             >
               <img src={p.image} alt={p.name} ></img>
               <div
