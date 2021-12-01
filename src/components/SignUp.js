84c98fd121f7bf2d8d857350b0f98f51c28d0246
anhnhivu <anhnhi160801@gmail.com>
2021-12-01 16:13:43 +0700

    Login/SignUp Page and NavBar Account and Cart Popup

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,14 +1,74 @@
-import React from 'react'
-import "../css/SignUpSignIn.css"
+import React, {Component} from 'react'
+import NavBar from './NavBar'
+import Footer from './Footer'
+import "../css/SignUp.css"
 
-const SignUp = () => {
-    return (
-        <div>
-            Sign Up
-        </div>
-    )
+export default class SignUp extends Component {
+    
+    render() {
+        return (
+            <div>
+                <NavBar/>
+
+                <br/> 
+                <div className="form-body">
+                    
+                <form className="form-signup">
+                    <div className="form-inner"> 
+                        <div className="h2-signup"> 
+                            <h2>Sign Up Now</h2>
+                        </div>
+                        
+
+
+                        <div className="form-group">
+                            <label className="" htmlFor="name1">Name<big className="required">*</big></label>
+                            <input className="" type="text" name="name1" id="name1" required/>
+                           
+                            
+                        </div>
+
+                        <div className="form-group">
+                            <label htmlFor="email1">Email<big className="required">*</big></label>
+                            <input type="text" name="email1" id="email1" required/>
+                        </div>
+                        
+                        <div className="form-group">
+                            <label htmlFor="password1">Password<big className="required">*</big></label>
+                            <input type="password" name="password1" id="password1" required/>
+                    
+                        </div>
+
+                        <div className="form-group">
+                            <label htmlFor="re-password">Confirm Password<big className="required">*</big></label>
+                            <input type="password" name="re-password" id="re-password" required/>
+                    
+                        </div>
+
+                        
+
+                        <br/>
+                        <br/>
+                        <br/>
+
+                        <div className="form-group">
+                            <button type="submit" className="login">
+                                Sign Up
+                            </button>
+                        </div>
+
+                        <div className="form-group">
+                            <button className="login-google">
+                            &nbsp; &nbsp; Sign up with Google
+                            </button>
+                        </div>
+
+                    </div>
+
+                </form>
+                </div>
+            <Footer/>
+            </div>
+        )
+    }
 }
-
-
-
-export default SignUp;
